bfc70c3ed4129d4dc107ecb3888a5637d618d0cc
LordKardo <86640310+LordKardo@users.noreply.github.com>
2022-08-03 09:49:56 +1200

    points system starting

--- game testig/Assets/itemcollector.cs
+++ game testig/Assets/itemcollector.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class itemcollector : MonoBehaviour
 {
+    private int points = 0;
+    [SerializeField] private Text pointstext;
+
     private void OnTriggerEnter2D(Collider2D collisions)
     {
         if (collisions.CompareTag("item"))
         {
             Destroy(collisions.gameObject);
+            points++;
+            pointstext.text = "Cherries: " + points;
         }
     }
 }
